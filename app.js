253e8f31640be4ed1275008a273f91518f81cbb7
Niloy-Sumon <niloyabidur@gmail.com>
2021-06-19 20:52:37 +0600

    some code edit

--- app.js
+++ app.js
@@ -10,40 +10,78 @@
 //     getmealData(inputMeal);
 //   });
 // };
-const addBtn = document.getElementById("search_button")
-addBtn.addEventListener('click', ()=>{
+// const addBtn = document.getElementById("search_button")
+// addBtn.addEventListener('click', ()=>{
+//     fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i")
+//   .then(res => res.json())
+//   .then(data =>{
+//       loadData(data)
+//     });
+// })
+
+const getWeatherData = ()=> {
     fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i")
-  .then(res => res.json())
-  .then(data =>{
-      loadData(data)
-    });
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data);
+            // loadData(data)
+            let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+                `;
+            });
+            // mealList.classList.remove('notFound');
+        } else{
+            html = "Sorry, we didn't find any meal!";
+            mealList.classList.add('notFound');
+        }
+
+        // mealList.innerHTML = html;
+        })
+}
+
+const searchBtn = document.getElementById('search_button');
+searchBtn.addEventListener('click', () => {
+    const inputCity = document.getElementById('meal').value;
+    getWeatherData(inputCity)
 })
+
+
+
 const loadData= (meal)=>{
     const div = document.getElementById("food-container")
-    meal.meals.forEach(element => {
-        // console.log(element);
-        const mealsDiv = document.createElement('div')
-        mealsDiv.className = 'meal'
-        const {strMeal, strMealThumb} = element
-        mealsDiv.innerHTML = `
-        <div>
-        <button onclick='displayContry("${meal}")'><img src="${strMealThumb}" alt=""></button>
-        </div>
-        <p>${strMeal}</p>
-        
-        `
-        div.appendChild(mealsDiv)
-    });
+    // meal.meals.forEach(element => {
+    //     // console.log(element);
+    //     const mealsDiv = document.createElement('div')
+    //     mealsDiv.className = 'meal'
+    //     const {strMeal, strMealThumb} = element
+    //     mealsDiv.innerHTML = `
+    //     <div>
+    //     <button onclick='displayMeal("${element.strMeal}")'> <img src="${strMealThumb}" alt="">
+    //     </button>
+    //     </div>
+    //     <p>${strMeal}</p>
+    //     `
+    //     div.appendChild(mealsDiv)
+    // });
+
+    
 }
-const displayContry = meal =>{
-    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal}`
-    console.log(url);
+const displayMeal = (meal) =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${meal}`
     fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data[0]);
-        // countryDetails(data[0])
-    })
+    .then(response => response.json())
+    .then(json => console.log(json))
   }
 
   const countryDetails = country =>{
